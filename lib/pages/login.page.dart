df0809cf6fc53296e4a9056e6328b4b4d93a2d1d
Samuel <samuelalves681@gmail>
2023-01-03 21:39:01 -0300

    home page search widget

--- lib/pages/login.page.dart
+++ lib/pages/login.page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shopping/pages/home.page.dart';
 import 'package:shopping/pages/signup.page.dart';
 
 class LoginPage extends StatelessWidget {
@@ -102,7 +103,12 @@ class LoginPage extends StatelessWidget {
                             'Sign In',
                             style: TextStyle(color: Colors.white),
                           ),
-                          onPressed: () {},
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => HomePage()));
+                          },
                         ),
                       )
                     ],
