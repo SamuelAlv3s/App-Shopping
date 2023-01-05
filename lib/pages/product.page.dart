3c84fc4d12977c79a3690c421e6593459192bba7
Samuel <samuelalves681@gmail>
2023-01-04 21:08:01 -0300

    feat: product details

--- lib/pages/product.page.dart
+++ lib/pages/product.page.dart
@@ -26,7 +26,34 @@ class ProductPage extends StatelessWidget {
               )
             ];
           },
-          body: Container()),
+          body: ListView(
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(top: 10, left: 10, right: 10),
+                child: Text(
+                  'Cadeira Gamer',
+                  style: TextStyle(fontSize: 26, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text('by Nike'),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'Details',
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.all(10),
+                child: Text(
+                  'O Lorem Ipsum é um texto modelo da indústria tipográfica e de impressão. O Lorem Ipsum tem vindo a ser o texto padrão usado por estas indústrias desde o ano de 1500, quando uma misturou os caracteres de um texto para criar um espécime de livro. Este texto não só sobreviveu 5 séculos, mas também o salto para a tipografia electrónica, mantendo-se essencialmente inalterada.',
+                ),
+              )
+            ],
+          )),
     );
   }
 }
