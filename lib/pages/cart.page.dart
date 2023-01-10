c4d9997e10ed05fa3639955399accf96aef9d004
Samuel <samuelalves681@gmail>
2023-01-09 21:31:34 -0300

    cart page list products

--- lib/pages/cart.page.dart
+++ lib/pages/cart.page.dart
@@ -13,8 +13,7 @@ class CartPage extends StatelessWidget {
       children: <Widget>[
         Expanded(
           child: Container(
-            color: Colors.red,
-            child: Text('Container 1'),
+            child: productList(),
           ),
         ),
         Container(
@@ -25,4 +24,85 @@ class CartPage extends StatelessWidget {
       ],
     ));
   }
+}
+
+Widget productList() {
+  return ListView(
+    children: <Widget>[
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+      productItem(),
+    ],
+  );
+}
+
+Widget productItem() {
+  return Container(
+    height: 120,
+    margin: EdgeInsets.all(5),
+    child: Row(children: <Widget>[
+      Container(
+        height: 100,
+        width: 100,
+        margin: EdgeInsets.all(10),
+        child: Image.asset(
+          'assets/product-1.png',
+          fit: BoxFit.fitWidth,
+        ),
+      ),
+      Padding(
+        padding: EdgeInsets.only(top: 20, left: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            Text('Título do Produto'),
+            Text(
+              '\$ 80.00',
+              style: TextStyle(color: Colors.green),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Container(
+              width: 120,
+              height: 30,
+              decoration: BoxDecoration(
+                  color: Colors.black12,
+                  borderRadius: BorderRadius.all(Radius.circular(5))),
+              child: Row(children: <Widget>[
+                Container(
+                  width: 40,
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    child: Text('+'),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  width: 40,
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    child: Text('1'),
+                    onPressed: () {},
+                  ),
+                ),
+                Container(
+                  width: 40,
+                  alignment: Alignment.center,
+                  child: TextButton(
+                    child: Text('-'),
+                    onPressed: () {},
+                  ),
+                ),
+              ]),
+            ),
+          ],
+        ),
+      )
+    ]),
+  );
 }
